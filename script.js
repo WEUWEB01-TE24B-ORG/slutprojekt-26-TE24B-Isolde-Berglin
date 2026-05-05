84730adc1f0af23735f767a36daa70e6dae36a47
Isolde Berglin <isolde.bergli@elev.ga.ntig.se>
2026-05-05 09:54:19 +0200

    försöker fixa layoiut för mobil o tablet

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let rightAnswer;
-function Correct(params) {
-    rightAnswer ++;
+function Correct() {
+    let rightAnswer;
+    rightAnswer = rightAnswer + 1;
+}
+function giveAnswer() {
+    let woo = rightAnswer;
+    document.querySelector("p.amount").textContent = rightAnswer + "/4"
 }
